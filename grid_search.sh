80bbdf8dc29d37178a136dbb626fb56c12a734d2
Copticon <ahmed.abdelsalam.ap@gmail.com>
2025-03-22 22:27:18 +0200

    reward settings evaluation

--- grid_search.sh
+++ grid_search.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
-# Grid search on R0, R1, R2, and sampler using GNU parallel
-# R0: 0.0001 to 0.5 (steps)
-# R1: 0.1 to 2 (steps)
-# R2: 0.5 to 3 (steps)
+# Grid search on R0, R1, R2, lr, and sampler using GNU parallel
 # sampler: normal or topological
 
 # Record the start time
 start_time=$(date +%s)
-
-# Function to generate a linspace using Python
-linspace() {
-    python3 -c "import numpy as np; print('\n'.join(map(str, np.linspace($1, $2, $3))))"
-}
-
-# Number of steps for each parameter
-steps=5
-echo "steps: $steps"
-
 # Generate arrays of parameter values
-r0_values=($(linspace 0.0001 0.5 "$steps"))
-r1_values=($(linspace 0.1 2 "$steps"))
-r2_values=($(linspace 0.5 3 "$steps"))
+lr_value=("0.005" "0.01" "0.05")
+r0_values=("0.00001" "0.00005" "0.0001" "0.0005" "0.001" "0.005" "0.01" "0.05" "0.1")
+r1=("1.0")
+r2=("3.0")
 sampler_values=("normal" "topological")
-
+ndim_values=("2" "3")
+height_values=("8" "64")
 # Generate a list of all parameter combinations and pipe to GNU parallel
-for sampler in "${sampler_values[@]}"; do
-    for r0 in "${r0_values[@]}"; do
-        for r1 in "${r1_values[@]}"; do
-            for r2 in "${r2_values[@]}"; do
+for ndim in "${ndim_values[@]}"; do
+    for height in "${height_values[@]}"; do
+        for sampler in "${sampler_values[@]}"; do
+            for r0 in "${r0_values[@]}"; do
                 # Output parameters separated by spaces
-                echo "$r0 $r1 $r2 $sampler"
+                echo "$r0 $r1 $r2 $sampler $ndim $height"
             done
         done
     done
-done | parallel -j 10 --colsep ' ' "python ./ter.py --R0 {1} --R1 {2} --R2 {3} --loss TB --growth_parameter 0.1 --n_trajectories 200000 --sampler {4}"
+done | parallel -j 6 --joblog job.log --colsep ' ' "python ./ter.py --R0 {1} --R1 {2} --R2 {3} --loss TB --growth_parameter 0.1 --n_trajectories 100000 --sampler {4} --ndim {5} --height {6} --wandb_project topological-sampler"
 # Record the end time and compute elapsed time
 end_time=$(date +%s)
 elapsed=$(( end_time - start_time ))
